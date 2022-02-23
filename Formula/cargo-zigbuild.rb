4151fa7052467457da1d707ace1d465b3f522e98
messense <messense@icloud.com>
2022-02-23 13:56:54 +0800

    cargo-zigbuild 0.6.4 (new formula)

---
+++ Formula/cargo-zigbuild.rb
@@ -0,0 +1,30 @@
+class CargoZigbuild < Formula
+  desc "Compile Cargo project with zig as linker"
+  homepage "https://github.com/messense/cargo-zigbuild"
+  url "https://github.com/messense/cargo-zigbuild/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "33ccefdcb370dfc656ce9e082489abf7b9113fe565310e73b0ef479f52f1aaf6"
+  license "MIT"
+  head "https://github.com/messense/cargo-zigbuild.git", branch: "main"
+
+  depends_on "rustup-init" => :test
+  depends_on "rust"
+  depends_on "zig"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # https://github.com/ziglang/zig/issues/10377
+    ENV.delete "CPATH"
+
+    system "#{Formula["rustup-init"].bin}/rustup-init", "-y", "--no-modify-path"
+    ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
+    system "rustup", "target", "add", "aarch64-unknown-linux-gnu"
+
+    system "cargo", "new", "hello_world", "--bin"
+    cd "hello_world" do
+      system "cargo", "zigbuild", "--target", "aarch64-unknown-linux-gnu.2.17"
+    end
+  end
+end
