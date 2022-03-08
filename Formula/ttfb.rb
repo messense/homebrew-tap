5b5f3b19fb6578afba16dcd3aad4eb620fe86370
messense <messense@icloud.com>
2022-03-08 10:48:23 +0000

    ttfb: update 1.3.0 bottle.

--- Formula/ttfb.rb
+++ Formula/ttfb.rb
@@ -7,9 +7,9 @@ class Ttfb < Formula
   head "https://github.com/phip1611/ttfb.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/messense/homebrew-tap/releases/download/ttfb-1.1.0"
-    sha256 cellar: :any_skip_relocation, catalina:     "67a049e29e685f952314ab301cb3bb77bff757354dd50a77c78ae4a12126c238"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "51b845f87cdcd5817a8c662a3623e1d52bec01365f77ccac96b49f050010767a"
+    root_url "https://github.com/messense/homebrew-tap/releases/download/ttfb-1.3.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "7b0d96530a32355c953671c1fb461b6f6c9a92009c3f3cbb487e4f5ceecd065c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "054859a821b01d3dd7236e71fbf106f7a694ded54ae6aaaed221b59d3b554c42"
   end
 
   depends_on "rust" => :build
